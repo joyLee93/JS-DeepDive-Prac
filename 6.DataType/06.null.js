9a9bdf7a1511f6a027f905b18eb12b9b8b4f104c
Joy Lee <rottenpear99@naver.com>
2022-06-16 17:58:20 +0900

    Data type-2

---
+++ 6.DataType/06.null.js
@@ -0,0 +1,13 @@
+//javascript 는 Character sensitive 
+// null != Null, NULL
+
+//null 은 변수에 값이 없다는 것을 의도적으로 명시 
+// 변수에 Null을 할당하는 것은 이전 참조하던 값을 더이상 참조하지 않겠다는 의미 
+
+var foo = 'Lee';
+foo = null;
+console.log('foo===>'+foo);
+
+
+var poo;
+console.log('poo===>'+poo); 
